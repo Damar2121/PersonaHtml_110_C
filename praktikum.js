575f3ddd5e5a1f22dc57204ed2d31ca125e26902
Damar2121 <152263707+Damar2121@users.noreply.github.com>
2025-04-22 16:17:19 +0700

    Add files via upload

---
+++ praktikum.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video() {
+    alert("ini video musik official Persona 3 Reload");
+});
+
+audio.addEventListener("click", function audio() {
+    alert("ini Audio official Persona 3 Reload");
+});
+
+gambar.addEventListener("click", function gambar() {
+    alert("ini gambar dari Game Persona 3 Reload");
+});
